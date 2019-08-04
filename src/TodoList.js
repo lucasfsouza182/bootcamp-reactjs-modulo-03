9a501b6751d58c2a987c90e758416bf93fa4b5ab
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-08-03 22:07:08 -0300

    Created addTodo fucntion

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-const TodoList = (props) => <ul>{ props.todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
+const TodoList = ({ todos, addTodo }) => (
+  <Fragment>
+    <ul>
+      { todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    </ul>
+    <button onClick={() => addTodo('Entrar na comunidade café')}>Adicionar</button>
+  </Fragment>
+)
 
 TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     text: PropTypes.string
@@ -15,4 +23,8 @@ const mapStateToProps = state => ({
   todos: state.todos
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
